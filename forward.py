1e418f23aa61f52735a9d35336e11d5297998120
zhishixiang <1015256551@qq.com>
2021-10-23 22:17:09 +0800

    加入对部分CQ码的处理

--- forward.py
+++ forward.py
@@ -4,12 +4,24 @@ from aiocqhttp import CQHttp, Event
 import httpx
 bot = CQHttp()
 
-group_whitelist = {1077550597:'gkd'}
-alias = 0000
+group_whitelist = {1077550597:'gkd'}    #群组白名单
+alias = 0000    #别名
 
 @bot.on_message('private')
 async def _(event: Event):
     msg = event['message']
+    if "CQ:image" in msg:
+        msg = "[图片]"
+    elif "CQ:record" in msg:
+        msg = "[语音]"
+    elif "CQ:share" in msg:
+        msg = "[链接]"
+    elif "CQ:music" in msg:
+        msg = "[音乐分享]"
+    elif "CQ:redbag" in msg:
+        msg = "[红包]"
+    elif "CQ:forward" in msg:
+        msg = "[合并转发]"
     nickName = event['sender']["nickname"]
     async with httpx.AsyncClient() as client:
         await client.post("https://tdtt.top/send",data={'title':'来自QQ的私聊消息','content':'%s:%s'%(nickName,msg),'alias':alias})
@@ -19,6 +31,18 @@ async def _(event: Event):
     groupId = event['group_id']
     if groupId in group_whitelist:
         msg = event['message']
+        if "CQ:image" in msg:
+            msg = "[图片]"
+        elif "CQ:record" in msg:
+            msg = "[语音]"
+        elif "CQ:share" in msg:
+            msg = "[链接]"
+        elif "CQ:music" in msg:
+            msg = "[音乐分享]"
+        elif "CQ:redbag" in msg:
+            msg = "[红包]"
+        elif "CQ:forward" in msg:
+            msg = "[合并转发]"
         nickName = event['sender']
         groupName = group_whitelist[event['group_id']]
         if nickName['card'] != "":
